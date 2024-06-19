ee58c481e334d40744652c89926011e20dba9787
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-06-19 16:51:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr
-  availability_zone = "us-west-1b"
+  availability_zone = "us-east-2b"
   tags = {
     Name = "subnet"
   }
@@ -102,10 +102,10 @@ variable "subnet_cidr" {
 
 # Creating EC2 instance
 resource "aws_instance" "wordpress_instance" {
-  ami                         = "ami-04639e6db24d3600d"
+  ami                         = "ami-0edf386e462400a51"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "project"
+  key_name                    = "keypair"
   vpc_security_group_ids      = ["${aws_security_group.wordpress_sg.id}"]
   subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
