9b170dd0137f65503d569d1eb384f6bdf4598b88
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-07-18 12:11:44 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt install -y docker
-sudo systemctl start docker
+sudo apt update
+sudo apt install -y docker.io
 sudo systemctl enable docker
+sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
